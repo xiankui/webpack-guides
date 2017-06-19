b3691fc23f2960aeaec00ae0a430251ab89b3894
zhuxiankui <zhuxiankui@zhuxiankuis-MacBook-Pro.local>
2017-06-19 11:15:48 +0800

    code-splitting-css

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,4 +1,5 @@
 const path = require('path')
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
 	devtool: 'cheap-module-source-map',
@@ -15,15 +16,21 @@ module.exports = {
 	module: {
 		rules: [{ // 对.js文件中导入的.css文件进行解析，并以样式的形式添加到head中
 			test: /\.css$/,
-			use: [
-				'style-loader',
-				'css-loader'
-			]
+			// use: [
+			// 	'style-loader',
+			// 	'css-loader'
+			// ],
+			use: ExtractTextPlugin.extract({
+			  use: 'css-loader'
+			})
 		}, { // 对css中的图片资源进行解析，必须！解析到output.path目录
 			test: /\.(png|svg|jpg|gif)$/,
 			use: [
 				'file-loader'
 			]
 		}]
-	}
+	},
+	plugins: [
+		new ExtractTextPlugin('styles.css'),
+	]
 }
